1e117db1112ef28d9bbcd81f1e22e84c00716ed4
David Weichselbaum <david.t.weichselbaum@gmail.com>
2025-05-02 15:09:13 +0200

    Code cleanup

--- agent_evolution.py
+++ agent_evolution.py
@@ -1,5 +1,6 @@
 import math
 import random
+from dataclasses import dataclass
 from time import time
 
 import pygame
@@ -11,6 +12,19 @@ from agents.environment import Environment
 from agents.constants import *
 from agents.agent import Agent
 
+
+@dataclass
+class Info():
+    dt: float
+    running: bool
+    food_timer: float
+
+    disable_rendering: bool
+    info_timer: float
+    selected_agent: Agent | None
+    start_time: float
+    simulation_seconds: float
+    clock: pygame.time.Clock
 
 def setup_env():
     env = Environment()
@@ -33,18 +47,34 @@ def setup_env():
     return env
 
 
-def handle_selected_agent(selected_agent, dead_agents, keys, env, fig, ax):
-    if not selected_agent or selected_agent in dead_agents:
-        selected_agent = env.agents[0] if env.agents else None
+
+def init_info_object():
+    dt = 1.0 / FPS_SIMULATION  # fixed simulation time step
+    clock = pygame.time.Clock()
+    start_time = time()
+    return Info(
+        dt=dt,
+        running=True,
+        food_timer=0.0,
+        disable_rendering=False,
+        info_timer=0.0,
+        selected_agent=None,
+        start_time=start_time,
+        simulation_seconds=0.0,
+        clock=clock,
+    )
+
+def handle_selected_agent(env, infos, keys, fig, ax):
+    if not infos.selected_agent:
+        infos.selected_agent = env.agents[0] if env.agents else None
 
         ax.clear()
-        if selected_agent:
-            selected_agent.net.get_graph_plot(ax)
+        if infos.selected_agent:
+            infos.selected_agent.net.get_graph_plot(ax)
         fig.canvas.draw()
         fig.canvas.flush_events()
 
-    controll_agent(keys, selected_agent)
-    return selected_agent
+    controll_agent(keys, infos.selected_agent)
 
 
 def controll_agent(keys, selected_agent):
@@ -70,87 +100,79 @@ def controll_agent(keys, selected_agent):
     selected_agent.possession_outputs = [acceleration, rotation]
 
 
-def print_infos(env, clock, simulation_seconds, start_time):
-    n_agents = len(env.agents)
-    n_foods = len(env.foods)
-    visualization_FPS = clock.get_fps()
-    simulation_time = timedelta(seconds=int(simulation_seconds))
-    wall_time = timedelta(seconds=int(time() - start_time))
-    print(f"Sim time: {simulation_time}    Wall time: {wall_time}    FPS: {visualization_FPS:.1f}    Agents: {n_agents}    Food: {n_foods}")
+def print_infos(env, infos):
+    infos.info_timer += infos.dt
+    if infos.info_timer >= INFO_INTERVAL:
+        infos.info_timer = 0.0
+
+        n_agents = len(env.agents)
+        n_foods = len(env.foods)
+        visualization_FPS = infos.clock.get_fps()
+        simulation_time = timedelta(seconds=int(infos.simulation_seconds))
+        wall_time = timedelta(seconds=int(time() - infos.start_time))
+        print(f"Sim time: {simulation_time}    Wall time: {wall_time}    FPS: {visualization_FPS:.1f}    Agents: {n_agents}    Food: {n_foods}")
 
 
-def simulation_step(dt, env, food_timer):
-    dead_agents, new_agents = env.update_agents(dt)
-    env.update_food(dt)
-    env.step(dt)
 
-    food_timer += dt
-    if food_timer >= FOOD_SPAWN_INTERVAL:
+def simulation_step(env, infos):
+    dead_agents, new_agents = env.update_agents(infos.dt)
+    if not env.agents:
+        infos.running = False
+
+    env.update_food(infos.dt)
+    infos.food_timer += infos.dt
+    if infos.food_timer >= FOOD_SPAWN_INTERVAL:
         env.spawn_food()
-        food_timer = 0.0
+        infos.food_timer = 0.0
 
-    return food_timer, dead_agents
+    if infos.selected_agent in dead_agents:
+        infos.selected_agent = None
 
-def visualization_step(screen, running, clock, selected_agent, dead_agents, env, fig, ax, disable_rendering):
+    env.step(infos.dt)
+    infos.simulation_seconds += infos.dt
+
+
+def visualization_step(screen, env, infos, fig, ax):
     keys = pygame.key.get_pressed()
 
     in_background = keys[pygame.K_b]
     in_foreground = keys[pygame.K_f]
     if in_background:
-        disable_rendering = True
+        infos.disable_rendering = True
     if in_foreground:
-        disable_rendering = False
-        selected_agent = None
+        infos.disable_rendering = False
+        infos.selected_agent = None
 
-    if not disable_rendering:
-        clock.tick(FPS_VISUALIZATION)
-        selected_agent = handle_selected_agent(selected_agent, dead_agents, keys, env, fig, ax)
+    if not infos.disable_rendering:
+        infos.clock.tick(FPS_VISUALIZATION)
+        handle_selected_agent(env, infos, keys, fig, ax)
 
         screen.fill(BACKGROUND_COLOR)
         env.draw_environment(screen)
-        if selected_agent:
-            selected_agent.draw_annotation(screen, env)
+        if infos.selected_agent:
+            infos.selected_agent.draw_annotation(screen, env)
     pygame.display.flip()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
-    return running, selected_agent, disable_rendering
-
-
+            infos.running = False
 
 
 def main():
     env = setup_env()
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    clock = pygame.time.Clock()
-
     plt.ion()  # Turn on interactive mode for matplotlib
     fig, ax = plt.subplots(figsize=(6, 6))
 
-    food_timer, info_timer = 0.0, 0.0
-    selected_agent = None
-    disable_rendering = False
-    running = True
-    start_time = time()
-    simulation_seconds = 0.0
-    dt = 1.0 / FPS_SIMULATION   # fixed simulation timestep
-    while running:
-        simulation_seconds += dt
+    infos = init_info_object()
 
-        food_timer, dead_agents = simulation_step(dt, env, food_timer)
-        if not env.agents:
-            running = False
+    while infos.running:
+        simulation_step(env, infos)
 
-        info_timer += dt
-        if info_timer >= INFO_INTERVAL:
-            print_infos(env, clock, simulation_seconds, start_time)
-            info_timer = 0.0
+        print_infos(env, infos)
 
-        running, selected_agent, disable_rendering = visualization_step(
-            screen, running, clock, selected_agent, dead_agents, env, fig, ax, disable_rendering)
+        visualization_step(screen, env, infos, fig, ax)
 
     pygame.quit()
     plt.close(fig)
